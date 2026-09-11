0e4045359e93894a183a8d969300cbf9e72e87bd
anneferris <annehutchinson407@gmail.com>
2026-09-10 18:19:00 -1000

    14

--- scripts/getdates.js
+++
@@ -1,5 +0,0 @@
-const footerSpan = document.querySelector(".footer-span");
-
-footerSpan.innerHTML = '${(new Date().getFullYear())}';
-
-document.getElementById("lastModified").textContent = document.lastModified;
