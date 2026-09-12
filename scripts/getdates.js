aabf40904f28c5eb33c3cfb3811e935c6993f330
anneferris <annehutchinson407@gmail.com>
2026-09-11 15:35:09 -1000

    17

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,6 +1,4 @@
-const currentYearSpan = document.querySelector("#currentyear");
-const today = new Date();
-currentYearSpan.textContent = today.getFullYear();
-
-const lastModified = document.querySelector("#lastModified");
-lastModified.textContent = 'Last Modification: ${document.lastModified}';
+document.addEventListener("DOMContentLoaded", () => {
+    const year = new Date().getFullYear();
+    document.getElementById('currentYear').textContent = year;
+})
